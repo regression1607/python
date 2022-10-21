855766b3b5ec3cd6d871dad2adb426509963fc6e
ghanshyam1584 <ghanshyamrajput1584@gmail.com>
2022-10-22 00:50:48 +0530

    python game
    egg catcher game

--- PATTERN.py
+++ PATTERN.py
@@ -1,12 +1,53 @@
-# This is the example of print simple pyramid pattern  
-n = int(input("Enter the number of rows"))  
-# outer loop to handle number of rows  
-for i in range(0, n):  
-    # inner loop to handle number of columns  
-    # values is changing according to outer loop  
-        for j in range(0, i + 1):  
-            # printing stars  
-            print("* ", end="")       
-  
-        # ending line after each row  
-        print() 
+n = 8
+ 
+# so this heart can be made n//2 part left,
+# n//2 part right, and one middle line
+# i.e; columns m = n + 1
+m = n+1
+ 
+# loops for upper part
+for i in range(n//2-1):
+    for j in range(m):
+         
+        # condition for printing stars to GFG upper line
+        if i == n//2-2 and (j == 0 or j == m-1):
+            print("*", end=" ")
+             
+        # condition for printing stars to left upper
+        elif j <= m//2 and ((i+j == n//2-3 and j <= m//4) \
+                            or (j-i == m//2-n//2+3 and j > m//4)):
+            print("*", end=" ")
+             
+        # condition for printing stars to right upper
+        elif j > m//2 and ((i+j == n//2-3+m//2 and j < 3*m//4) \
+                           or (j-i == m//2-n//2+3+m//2 and j >= 3*m//4)):
+            print("*", end=" ")
+             
+        # condition for printing spaces
+        else:
+            print(" ", end=" ")
+    print()
+ 
+# loops for lower part
+for i in range(n//2-1, n):
+    for j in range(m):
+         
+        # condition for printing stars
+        if (i-j == n//2-1) or (i+j == n-1+m//2):
+            print('*', end=" ")
+             
+        # condition for printing GFG
+        elif i == n//2-1:
+             
+            if j == m//2-1 or j == m//2+1:
+                print('G', end=" ")
+            elif j == m//2:
+                print('F', end=" ")
+            else:
+                print(' ', end=" ")
+                 
+        # condition for printing spaces
+        else:
+            print(' ', end=" ")
+             
+    print()
